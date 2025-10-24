21f20956d6bf460fdcdc34d122f52f63bbd7a383
sayranovv <sayranoveldar@gmail.com>
2025-10-24 16:07:46 +0500

    Add layouts

---
+++ client/app/layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { LucideAreaChart } from 'lucide-vue-next'
+
+</script>
+<template>
+  <UHeader title="Grade View ETIS">
+    <template #left>
+      <LucideAreaChart class="size-8" />
+      <h1 class="text-xl font-bold">Grade View ETIS</h1>
+    </template>
+    <template #right>
+      <SelectTermModal/>
+      <LogoutConfirmationModal />
+    </template>
+  </UHeader>
+  <div class="my-8">
+    <slot />
+  </div>
+  <UFooter />
+</template>
