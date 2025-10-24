e8eebfbdbe8951120be82ce11e82fa2ef3be213f
sayranovv <sayranoveldar@gmail.com>
2025-10-24 22:24:30 +0500

    Fix CORS error

--- server/main.py
+++ server/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
         "http://localhost:3001",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
+        "https://grade-view-etis.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
