637b4a961b7a6fae281f820edb52f7a09bcfa153
sayranovv <sayranoveldar@gmail.com>
2025-10-24 16:08:02 +0500

    Add components

---
+++ client/app/components/LineChartTerms.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps<{
+  data: {
+    term: number
+    score: number
+  }[]
+}>()
+
+const categories = {
+  score: {
+    name: 'Средний балл за семестр',
+    color: '#3b82f6',
+  },
+}
+
+const xFormatter = (i: number) => (props.data && props.data[i] ? props.data[i].term : '')
+</script>
+
+<template>
+    <LineChart
+        v-if="data && data.length"
+        :data="data"
+        :categories="categories"
+        :height="300"
+        :xFormatter="xFormatter"
+        xLabel="Семестр"
+        yLabel="Средний балл"
+    />
+</template>
---
+++ client/app/components/SelectTermModal.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import * as v from 'valibot'
+import type { FormSubmitEvent } from '@nuxt/ui'
+import { useUserStore} from "~/stores/user";
+
+const analysis = useAnalysis()
+const userStore = useUserStore()
+
+const terms = ref<string[]>(['Все'])
+const isModalOpen = ref<boolean>(false)
+const loading = ref<boolean>(false)
+
+const openModal = () => isModalOpen.value = true
+
+const closeModal = () => isModalOpen.value = false
+
+const schema = v.object({
+  selectTerm: v.pipe(v.string(), v.minLength(1, 'Выберите семестр из списка'))
+})
+
+type Schema = v.InferOutput<typeof schema>
+
+const state = reactive({
+  selectTerm: ''
+})
+
+const onSubmit = async (event: FormSubmitEvent<Schema>) => {
+  loading.value = true
+  const user = JSON.parse(sessionStorage.getItem('user')!)
+
+  await analysis.analyze({
+    username: user.username,
+    password: user.password,
+    term: event.data.selectTerm === 'Все' ? '' : event.data.selectTerm
+  })
+
+  loading.value = false
+  closeModal()
+}
+
+onMounted(() => {
+  const userTerms = userStore.user?.terms.map(String)
+
+  if (Array.isArray(userTerms)) {
+    terms.value.push(...userTerms)
+  }
+})
+</script>
+
+<template>
+  <UButton label="Выбрать семестр" @click="openModal" class="cursor-pointer" />
+    <UModal v-model:open="isModalOpen" title="Выберите семестр/триместр для анализа">
+      <template #body>
+        <UForm :schema="schema" :state="state" @submit="onSubmit" class="space-y-4">
+          <UFormField
+              required
+              label="Выберите доступный семестр"
+              name="selectTerm"
+          >
+            <USelect v-model="state.selectTerm" :items="terms" class="w-full cursor-pointer" />
+          </UFormField>
+          
+          <UButton :loading="loading" loading-icon="i-lucide-loader" type="submit" label="Анализ" class="flex justify-center items-center w-full cursor-pointer" />
+        </UForm>
+      </template>
+    </UModal>
+</template>
